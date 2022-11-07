aa885daee6d4e1c87d34f047697daa2623503535
nhat <nhatche42@gmail.com>
2022-11-07 11:44:01 +0700

    complete

--- main/main.py
+++ main/main.py
@@ -62,14 +62,7 @@ def check_queue(queue, csv):
     print('in checkqueue: ', data)
     csv.handle_one_informatio(data)
 
-if __name__ == '__main__':
-
-
-  processes = []
-  queue1 = Queue()
-  csv = CsvHandle()
-
-  #create and run 20 threads
+def run(queue1, csv):
   for i in range(20):
     p = Process(target=getData, args=(queue1, list_id[i],))
     p.start()
@@ -96,4 +89,44 @@ if __name__ == '__main__':
   #join 20 threads
   for process in processes_browser:
     process.join()
+
+if __name__ == '__main__':
+
+
+  processes = []
+  queue1 = Queue()
+  csv = CsvHandle()
+
+  while(True):
+    run(queue1, csv)
+    time.sleep(5)
+  # #create and run 20 threads
+  # for i in range(20):
+  #   p = Process(target=getData, args=(queue1, list_id[i],))
+  #   p.start()
+  #   processes.append(p)
+
+  # #join 20 threads
+  # for process in processes:
+  #   process.join()
+
+  # #thread to checkQueue
+  # queue_thread = Process(target=check_queue, args=(queue1,csv,))
+  # queue_thread.start()
+  # queue_thread.join()
+
+
+  # processes_browser = []
+  # max_time = 20
+  # for i in range(20):
+  #   # additional_time = i/50
+  #   p1 = Process(target=open_close, args=(list_id[i], i ,))
+  #   processes_browser.append(p1)
+  #   p1.start()
+    
+  # #join 20 threads
+  # for process in processes_browser:
+  #   process.join()
+
+  
 
